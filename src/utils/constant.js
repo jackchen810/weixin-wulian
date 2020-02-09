80e6c079cb521f76c3f4e46693a3d79cb2650518
jackchen810 <jack_chen_mail@163.com>
2020-02-10 00:42:06 +0800

    chenzejun initlize

---
+++ src/utils/constant.js
@@ -0,0 +1,29 @@
+/**
+ * 用户code 换取 session_key
+ * @type {String}
+ */
+export const USER_SPECICAL_INFO = "userSpecialInfo";
+
+/**
+ * 用户信息
+ * @type {String}
+ */
+export const USER_INFO = "userInfo";
+
+/**
+ * 系统信息
+ * @type {String}
+ */
+export const SYSTEM_INFO = "systemInfo";
+
+
+export const ADDRESS_ID = "addressId";
+
+export const SEL_CLASS_CODE = "selClassCode";
+
+export const DEVICE_NAME = "deviceName";
+export const DEVUNIT_NAME = "devunitName";
+export const CHANNEL_NAME = "channelName";
+export const CUR_DEVICE_NAME = "currentDeviceName";
+export const PROJECT_NAME = "projectName";
+export const PAGE_CONTEXT = "pageContext";
